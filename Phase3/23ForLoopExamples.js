a4a5e0a4b42a1853acd7662293d34a1e6822ed07
Arslan <imarslan7061@gmail.com>
2026-02-02 23:24:52 +0500

    for loop example

---
+++ Phase3/23ForLoopExamples.js
@@ -0,0 +1,8 @@
+// for loop examples
+
+let total = 0;
+let num = 10
+for (let i = 1; i <= num; i++) {
+  total += i; // total = total + i
+}
+console.log("The total sum from 1 to 10 is: " + total);
