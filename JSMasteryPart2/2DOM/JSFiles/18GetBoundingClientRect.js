5785e41c8d9b526b618ef3d9ecb81c63aed84658
Muhammad Arslan <imarslan7061@gmail.com>
2026-02-17 00:46:13 +0500

    Update 18GetBoundingClientRect.js

--- JSMasteryPart2/2DOM/JSFiles/18GetBoundingClientRect.js
+++ JSMasteryPart2/2DOM/JSFiles/18GetBoundingClientRect.js
@@ -1,6 +1,9 @@
 // how to get the dimension of element
 // height width 
 const sectionTodo = document.querySelector(".section-todo");
-const info = sectionTodo.getBoundingClientRect();
+// const info = sectionTodo.getBoundingClientRect().bottom;
+// const info = sectionTodo.getBoundingClientRect().height;
+// const info = sectionTodo.getBoundingClientRect().left;
+const info = sectionTodo.getBoundingClientRect().right;
 console.log(info);
 
